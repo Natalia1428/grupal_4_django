97032d0f7cb543b584052c657c78bfb5c9e7dec1
camilo-monsalves <camilo.dbm2@gmail.com>
2023-08-01 20:14:04 -0400

    se implementó login, verificar sesión activa, logout.

--- TLVApp/usuarios/urls.py
+++ TLVApp/usuarios/urls.py
@@ -6,6 +6,7 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('registro/', views.registro_usuario, name='usuario-registro'),
-    path('login/', auth_views.LoginView.as_view(template_name='usuarios/login.html'), name='usuario-login'),
+    path('login/', views.custom_login, name='usuario-login'),
+    path('logout/', views.custom_logout, name='usuario-logout'),
     path('perfil/', auth_views.LoginView.as_view(template_name='usuarios/perfil.html'), name='usuario-perfil')
 ]
--- TLVApp/usuarios/views.py
+++ TLVApp/usuarios/views.py
@@ -1,3 +1,4 @@
+
 from django.shortcuts import render, redirect
 #1) importar messages
 from django.contrib import messages
@@ -8,44 +9,84 @@ from .forms import RegistrarUsuarioForm
 #3) importar login required
 from django.contrib.auth.decorators import login_required
 
+# importar logout, login, authenticate
+from django.contrib.auth import logout, login, authenticate
+
+# importar AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm
+
 def registro_usuario(request):
-    if request.method == 'POST':
-        formulario_post = RegistrarUsuarioForm(request.POST)
-        #validar formulario
-        if formulario_post.is_valid():
-            usuario = formulario_post.save(commit=False)
-            
-            username = formulario_post.cleaned_data['username']
-
-            # mensaje de éxito
-            messages.success(request, f'Cuenta registrada exitosamente para el usuario {username}')
-
-            # guardar formulario
-            formulario_post.save()
-            
-            # redireccionar a la página de log in.
-            return redirect('usuario-registro')
-        else:
-            messages.error(request, f'Hubo un error en el proceso de registro del usuario  {username}')
-
-    formulario = RegistrarUsuarioForm()
+    # usuarios logeados no pueden registrar una cuenta nueva.
+    if request.user.is_authenticated:
+        return redirect('home')
     
-    return render(request, 'usuarios/registro.html', {'formulario':formulario})
+    if request.method == "POST":
+        form = RegistrarUsuarioForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            username = form.cleaned_data['username']
+            login(request, user)
+            messages.success(request, f'Nueva cuenta creada: {user.username}')
+            return redirect('home')
+        else:
+            for error in list(form.errors.values()):
+                print(request, error)
+    else:
+        form = RegistrarUsuarioForm()
+    
+    return render(
+        request = request,
+        template_name="usuarios/registro.html",
+        context={"form":form}
+    )
 
-# *** IMPORTANTE implementar en settings.py LOGIN_REDIRECT_URL ***
+# *** IMPORTANTE implementar en settings.py LOGIN_REDIRECT_URL = "" ***
+# @login_required
+# def perfil(request):
+#     if request.method == 'POST':
+#         formulario_usuario = RegistrarUsuarioForm(request.POST, isinstance=request.user)
+
+#         # validar formulario.
+#         if formulario_usuario.is_valid():
+#             usuario = formulario_usuario.save(commit=False)
+
+#             username = formulario_usuario.cleaned_data['username']
+
+#             formulario_usuario.save()
+
+#     formulario = RegistrarUsuarioForm()
+
+#     return render(request, 'usuarios/login.html', {'formulario':formulario})
+
 @login_required
-def perfil(request):
-    if request.method == 'POST':
-        formulario_usuario = RegistrarUsuarioForm(request.POST, isinstance=request.user)
+def custom_logout(request):
+    logout(request)
+    messages.info(request, 'Sesión finalizada correctamente!')
+    return redirect('home')
 
-        # validar formulario.
-        if formulario_usuario.is_valid():
-            usuario = formulario_usuario.save(commit=False)
+def custom_login(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    
+    if request.method == "POST":
+        form = AuthenticationForm(request=request, data=request.POST)
+        if form.is_valid(): # si el formulario de login es válido.
+            user = authenticate(
+                username = form.cleaned_data['username'],
+                password = form.cleaned_data['password']
+            )
+            if user is not None:
+                login(request, user)
+                messages.success(request, f'Bienvenido {user.username}')
+                return redirect('home')
+        else:
+            for error in list(form.errors.values()):
+                messages.error(request, error)
+    
+    form = AuthenticationForm()
 
-            username = formulario_usuario.cleaned_data['username']
-
-            formulario_usuario.save()
-
-    formulario = RegistrarUsuarioForm()
-
-    return render(request, 'usuarios/login.html', {'formulario':formulario})
+    return render(
+        request=request,
+        template_name='usuarios/login.html',
+        context={'form':form}
+    )
